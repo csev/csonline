8443efc6090d41d0a5525a1b000dd9ab7dbc5782
Charles Severance <csev@umich.edu>
2012-12-18 20:22:12 -0800

    Make local copies of the static files in dev mode. Move the course info into a non-git file.

---
+++ data-dist.php
@@ -0,0 +1,9 @@
+<?php
+
+$COURSE_LIST = Array(
+"playground" => Array(
+  'key' => "online.dr-chuck.com",
+  'secret' => "19869a2f92b8e1f965deadc32850742d",
+  'endpoint' => "http://moodle.dr-chuck.com/moodle/local/ltiprovider/tool.php?id=3"
+)
+);
