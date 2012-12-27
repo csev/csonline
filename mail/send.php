5c38b12e0ecc196e9d0987edbad4d4bba572ab98
Charles Severance <csev@umich.edu>
2012-12-27 01:05:43 -0500

    Misc little things

--- mail/send.php
+++ mail/send.php
@@ -1,10 +1,18 @@
 <?php
 $to = "csev@umich.edu";
 $subject = "Test mail";
-$message = "Hello! This is a simple email message.";
+$message = "Hello! This is a simple email message.
+
+You can manage your mail preferences at http://online.dr-chuck.com/profile.php";
 $from = "no-reply@mail.dr-chuck.com";
-$headers = "From:" . $from;
+// $headers = "From:" . $from;
+$headers = 'From: '.$from."\n" .
+    'Reply-To: ' . $from . "\n" .
+    'List-Unsubscribe: <http://online.dr-chuck.com/unsubscribe.php?id=15&token=b61a7643c655d3bc66a1609d2110abdad0ec568e>' . "\n" .
+    'X-Mailer: PHP/' . phpversion();
+
 mail($to,$subject,$message,$headers);
 echo "Mail Sent.";
+echo $headers;
 ?>
 
