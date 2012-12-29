07c2f507ef09f048160b8cd2760fbfed89b8d394
Charles Severance <csev@umich.edu>
2012-12-29 14:53:24 -0500

    Add paging to crud

--- admin/crud/index.php
+++ admin/crud/index.php
@@ -2,6 +2,7 @@
 require_once "crudstart.php";
 require_once "../../db.php";
 require_once "../../sqlutil.php";
+$page_length = 20;
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,18 +24,103 @@ if ( isset($_SESSION['success']) ) {
     unset($_SESSION['success']);
 }
 
+$search = '';
+if ( isset($_GET['search_text']) ) {
+    $search = $_GET['search_text'];
+    if ( strlen($search) < 1 ) $search = '';
+}
+$page_start = 0;
+if ( isset($_GET['page_start']) ) {
+    $page_start = $_GET['page_start'] + 0;
+}
+if ( $page_start < 0 ) $page_start = 0;
+
+$order_by = '';
+if ( isset($_GET['order_by']) ) { 
+    $order_by = $_GET['order_by'];
+}
+?>
+<?php
 $sql = "SELECT ";
-echo '<table border="1">'."\n<tr>";
 for($i=0; $i < $info[0]; $i++ ) {
     $sql.= $fields[$i].', ';
+}
+$sql .= "id FROM $table";
+
+if ( strlen($search) > 0 ) {
+    $searchtext = '';
+    for($i=0; $i < $info[0]; $i++ ) {
+        if ( $i > 0 ) $searchtext .= " OR ";
+        $searchtext .= $fields[$i]." LIKE '%".mysql_real_escape_string($search)."%'";
+    }
+    $sql .= " WHERE " . $searchtext;
+}
+
+if ( strlen($order_by) > 0 ) {
+    $sql .= " ORDER BY ".mysql_real_escape_string($order_by)." ";
+}
+
+if ( $page_start <= 0 ) {
+    $sql .= " LIMIT ".($page_length+1);
+} else {
+    $sql .= " LIMIT ".$page_start.", ".($page_length+1);
+}
+
+// echo($sql);echo("<hr>");
+
+$count = 0;
+$result = run_mysql_query($sql);
+$rows = Array();
+while ( $row = mysql_fetch_row($result) ) {
+    array_push($rows, $row);
+    $count = $count + 1;
+}
+
+$have_more = false;
+if ( $count > $page_length ) {
+    $have_more = true;
+    $count = $page_length;
+}
+
+echo('<div style="float:right">');
+if ( $page_start > 0 ) {
+    echo('<form style="display: inline">');
+    echo('<input type="submit" value="Back">');
+    echo('<input type="hidden" name="table" value="'.htmlentities($table).'">');
+    echo('<input type="hidden" value="'.$search.'" name="search_text">');
+    $page_back = $page_start - $page_length;
+    if ( $page_back < 0 ) $page_back = 0;
+    echo('<input type="hidden" value="'.$page_back.'" name="page_start">');
+    echo("</form>\n");
+}
+if ( $have_more ) {
+    echo('<form style="display: inline">');
+    echo('<input type="submit" value="Next"> ');
+    echo('<input type="hidden" name="table" value="'.htmlentities($table).'">');
+    echo('<input type="hidden" value="'.$search.'" name="search_text">');
+    $page_next = $page_start + $page_length;
+    echo('<input type="hidden" value="'.$page_next.'" name="page_start">');
+    echo("</form>\n");
+}
+echo("</div>\n");
+echo('<form>');
+echo('<input type="hidden" name="table" value="'.htmlentities($table).'">');
+echo('<input type="text" value="'.htmlentities($search).'" name="search_text">');
+echo('<input type="hidden" value="'.$page_start.'" name="page_start">');
+echo('<input type="submit" value="Search">');
+echo("</form>\n");
+
+echo('<table border="1">');
+echo("<tr>\n");
+
+for($i=0; $i < $info[0]; $i++ ) {
     echo("<th>".ucfirst($fields[$i])."</th>\n");
 }
 echo("<th>Action</th>\n");
 echo("</tr>\n"); 
-$sql .= "id FROM $table";
 
-$result = run_mysql_query($sql);
-while ( $row = mysql_fetch_row($result) ) {
+for ($pos=0; $pos<$count; $pos++ ) {
+    $row = $rows[$pos];
     echo("<tr>");
     for($i=0; $i < $info[0]; $i++ ) {
         echo("<td>");
