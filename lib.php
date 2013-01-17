0731ebd565b5e4b15f342def11b2c918baa5e5a5
Charles Severance <csev@umich.edu>
2013-01-17 15:21:53 -0500

    Too bad I just did not have PHP 5.4 instead of this :)

--- lib.php
+++ lib.php
@@ -12,7 +12,7 @@ function isCli() {
     return "cli" == php_sapi_name();
 }
 
-// Fix this after PHP 5.4
+// Fix after PHP 5.4
 function htmlencode($string) {
     return htmlentities($string, ENT_COMPAT, 'UTF-8');
 }
