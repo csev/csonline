efeaf81df0d812d18ace1af9c954f947ae0f0ca5
Charles Severance <csev@umich.edu>
2024-04-19 11:18:43 -0400

    Update CC4E

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 </p>
 <h2>Programming For Everybody</h2>
 <p>
-This is a series of courses designed to take students from knowing nothing about programming to the point where they have the 
+This is a series of courses designed to take students from knowing nothing about programming to the point where they have the
 confidence to consider a career as a software developer, designer, or take further education.
 </p>
 <ul>
@@ -57,42 +57,46 @@ confidence to consider a career as a software developer, designer, or take furth
 - A course designed to make us all better citizens of the Internet.  Available on Coursera.
 </p></li>
 <li><p>
-<a href="https://www.py4e.com/" target="_blank">Python for Everybody (PY4E)</a>- The world's most successful online Python course.  Available on Coursera, 
-edX, FutureLearn, FreeCodeCamp, and
+<a href="https://www.py4e.com/" target="_blank">Python for Everybody (PY4E)</a>- The world's most successful online Python course.  Available on Coursera,
+edX, FreeCodeCamp, and
 <a href="https://online.umich.edu/series/python-for-everybody/" target="_blank">free to University of Michigan Students</a>.
 </p></li>
 <li><p>
 <a href="https://www.dj4e.com/" target="_blank">Django for Everybody (DJ4E)</a> - Covers
-Django, HTML, CSS, SQL, JavaScript, BootStrap, and JQuery.  Available on Coursera, edX, FutureLearn, FreeCodeCamp and
+Django, HTML, CSS, SQL, JavaScript, BootStrap, and JQuery.  Available on Coursera, edX, FreeCodeCamp and
 <a href="https://online.umich.edu/series/django/" target="_blank">free to University of Michigan Students</a>.
 </p></li>
 <li><p>
 <a href="https://www.wa4e.com/" target="_blank">Web Applications for Everybody (WA4E)</a> - Covers
-HTML, CSS, PHP, SQL, JavaScript, BootStrap, and JQuery.  Available on Coursera, FreeCodeCamp, and 
+HTML, CSS, PHP, SQL, JavaScript, BootStrap, and JQuery.
+Available on Coursera, FreeCodeCamp, and
 <a href="https://online.umich.edu/series/web-applications-for-everybody/" target="_blank">
 free to University of Michigan Students</a>.
 </p></li>
 <li><p>
-<a href="https://www.pg4e.com" target="_blank">PostgreSQL for Everybody (PG4E)</a> 
+<a href="https://www.pg4e.com" target="_blank">PostgreSQL for Everybody (PG4E)</a>
 A series of four courses covering PostgreSQL.  Available on Coursera, edX,
 and
 <a href="https://online.umich.edu/series/postgresql-for-everybody/" target="_blank">free to University of Michigan Students</a>.
 </p></li>
-<li><p>Under Development:
-<a href="https://www.cc4e.com" target="_blank">C Programming - A Historical Perspective (CC4E)</a> 
+<li><p>
+<a href="https://www.cc4e.com" target="_blank">C Programming for Everybody (CC4E)</a>
 A course looking at the C language and its profound effect on modern computing and Computer Science.
+Available on Coursera, FreeCodeCamp, and
+<a href="https://online.umich.edu/series/c-programming-for-everybody/" target="_blank">
+free to University of Michigan Students</a>.
 </p></li>
 <li><p>
-Future course topics include Computer Architecture and Java.
+Future course topics include Computer Architecture, JavaScript and Java.
 </p></li>
 </ul>
 </p>
 <p>
-The University of Michigan has a wide range of additional open online courses available at 
+The University of Michigan has a wide range of additional open online courses available at
 <a href="https://online.umich.edu" target="_blank">https://online.umich.edu</a>.
 </p>
 <p>
-The materials in these courses are available under a Creative Commons License to allow for teachers 
+The materials in these courses are available under a Creative Commons License to allow for teachers
 to make use of these materials in their own courses.
 Some of these
 courses are made available as part of a packaged curriculum to small Liberal Arts universities for direct adoption through
