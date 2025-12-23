4bff8d9de278fa8e5ccf8f9ec183b021b7ad8ab1
Charles Severance <csev@umich.edu>
2025-12-23 00:17:36 -0500

    Updates.

--- about.php
+++ about.php
@@ -34,7 +34,7 @@
 <a href="https://twitter.com/drchuck" target="_blank"><img src="https://en.gravatar.com/avatar/2d0a2f518066c5fd09d757a289b54307?s=80" height="40" width="40" alt="Logo image"></a>
 <span class="hidden-phone">Dr. Chuck Online</span>
 <span class="visible-phone"><small>Dr.C.O</small></span>
-<span class="hidden-phone hidden-tablet" style="color: grey;font-size: 14px; vertical-align: middle">MOOCs, Open Standards, Open Source and OERs</span></span></h1>
+<span class="hidden-phone hidden-tablet" style="color: grey;font-size: 14px; vertical-align: middle">Open Standards, Open Source and Open Education</span></span></h1>
 <div class="navbar" style="bgcolor: orange">
   <div class="navbar-inner">
     <ul class="nav nav-pills">
@@ -46,30 +46,34 @@
   </div>
 </div>
 <div class="hidden-phone hidden-tablet" style="width: 560; float: right; margin:10px;">
-<iframe width="450" height="253" src="https://www.youtube.com/embed/hRNFBhEykcY" frameborder="0" allowfullscreen></iframe>
+<iframe width="400" height="225" style="float: right;" src="https://www.youtube.com/embed/UjeNA_JtXME" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 <p>
 This is the web site of the "University of Dr. Chuck".  I teach a number of free/online/open courses based on
 free and open materials and this site is the course catalog for my free courses.  Some of these courses
-are available on mainstream MOOC platforms and other courses are provided using my own open source MOOC platform
+are available on mainstream educational technology  platforms and other courses are provided using my own open source platform
 called <a href="https://www.tsugi.org/" target="_blank">Tsugi</a>
 <p>
-The purpose of me building and deploying my own MOOC infrastructure is to explore some of the areas of teaching and learning 
-that I feel are missing from the mainstream MOOC platforms and efforts.  I think that these platforms will eventually address
+The purpose of me building and deploying my own EdTech infrastructure is to explore some of the areas of teaching and learning 
+that I feel are missing from the mainstream platforms and efforts.  I think that these platforms will eventually address
 the issues near and dear to my heart but not quickly enough for me.
 </p>
 <p>
-I also like to use my own platforms to show feasibility of my ideas in hopes that mainstream MOOC platforms 
-like Coursera and edX can learn from my experiments.
+I also like to use my own platforms to show feasibility of my ideas in hopes that mainstream platforms 
+like Canvas, Coursera and edX can learn from my experiments.
 </p>
 <p>
-Some of these courses are official offerings of the 
-<a href="https://online.umich.edu" target="_blank">University of Michigan</a>
-(i.e. though Coursera or edX) but some of these
-offerings are just from me - the "University of Dr. Chuck".
+I use these materials to teach many of my on campus courses as a University of Michigan
+School of Informaiotn faculty member.   These courss in my platform at not 
+University of Michigan offerings and do not confer any University of Michigan
+credit.
+</p>
 <p>
-Charles Severance - 
-Mon Jul 29 09:30:50 EDT 2019
+If you would like to support Dr. Chuck's efforts with a small subscription or donation,
+please see my 
+<a href="https://patreon.dr-chuck.com/" target="_blank">Patreon</a> page.  You can also
+subscribe to my <a href="https://youtube.dr-chuck.com" target="_blank">YouTube channel</a>
+or just go to my chanel and use the "Thanks" feature on a video to leave me a tip.
 </p>
 </div>
 </body>
--- index.php
+++ index.php
@@ -23,6 +23,49 @@
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
+    <style>
+        .course-card-link {
+            display: block;
+            text-decoration: none;
+            color: inherit;
+            cursor: pointer;
+        }
+        .course-card-link:hover {
+            text-decoration: none;
+            color: inherit;
+        }
+        .course-card {
+            margin-bottom: 30px;
+            height: 100%;
+            transition: transform 0.2s;
+            border: 1px solid #ddd;
+            max-width: 320px;
+        }
+        .course-card:hover {
+            transform: translateY(-5px);
+            box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+        }
+        .course-card img {
+            width: 100%;
+            height: auto;
+            display: block;
+        }
+        .course-card .caption {
+            padding: 15px;
+        }
+        .course-card h3 {
+            margin-top: 0;
+            font-size: 20px;
+        }
+        .course-card p {
+            margin-bottom: 10px;
+            color: #666;
+            font-size: 14px;
+        }
+        .course-card .btn {
+            margin-top: 10px;
+        }
+    </style>
 
 </head>
 <body style="padding: 0px 10px 0px 10px">
@@ -32,7 +75,7 @@
 <a href="https://twitter.com/drchuck" target="_blank"><img src="https://en.gravatar.com/avatar/2d0a2f518066c5fd09d757a289b54307?s=80" height="40" width="40" alt="Logo image"></a>
 <span class="hidden-phone">Dr. Chuck Online</span>
 <span class="visible-phone"><small>Dr.C.O</small></span>
-<span class="hidden-phone hidden-tablet" style="color: grey;font-size: 14px; vertical-align: middle">MOOCs, Open Standards, Open Source and OERs</span></span></h1>
+<span class="hidden-phone hidden-tablet" style="color: grey;font-size: 14px; vertical-align: middle">Open Standards, Open Source and Open Education</span></span></h1>
 <div class="navbar" style="bgcolor: orange">
   <div class="navbar-inner">
     <ul class="nav nav-pills">
@@ -43,58 +86,111 @@
     </ul>
   </div>
 </div>
-<iframe width="400" height="225" style="float: right;" src="https://www.youtube.com/embed/UjeNA_JtXME" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <p>
 </p>
-<h2>Programming For Everybody</h2>
+<h2>Programming For Everybody - Becoming a Master Programmer</h2>
 <p>
 This is a series of courses designed to take students from knowing nothing about programming to the point where they have the
-confidence to consider a career as a software developer, designer, or take further education.
+skills for an entry level job or internship in a software development orginization.
 </p>
-<ul>
-<li><p>
-<a href="https://ihts.pr4e.com/" target="_blank">Internet History, Technology, and Security</a> (IHTS)
-- A course designed to make us all better citizens of the Internet.  Available on Coursera.
-</p></li>
-<li><p>
-<a href="https://www.py4e.com/" target="_blank">Python for Everybody (PY4E)</a>- The world's most successful online Python course.  Available on Coursera,
-edX, FreeCodeCamp, and
-<a href="https://online.umich.edu/series/python-for-everybody/" target="_blank">free to University of Michigan Students</a>.
-</p></li>
-<li><p>
-<a href="https://www.dj4e.com/" target="_blank">Django for Everybody (DJ4E)</a> - Covers
-Django, HTML, CSS, SQL, JavaScript, BootStrap, and JQuery.  Available on Coursera, edX, FreeCodeCamp and
-<a href="https://online.umich.edu/series/django/" target="_blank">free to University of Michigan Students</a>.
-</p></li>
-<li><p>
-<a href="https://www.wa4e.com/" target="_blank">Web Applications for Everybody (WA4E)</a> - Covers
-HTML, CSS, PHP, SQL, JavaScript, BootStrap, and JQuery.
-Available on Coursera, FreeCodeCamp, and
-<a href="https://online.umich.edu/series/web-applications-for-everybody/" target="_blank">
-free to University of Michigan Students</a>.
-</p></li>
-<li><p>
-<a href="https://www.pg4e.com" target="_blank">PostgreSQL for Everybody (PG4E)</a>
-A series of four courses covering PostgreSQL.  Available on Coursera, edX,
-and
-<a href="https://online.umich.edu/series/postgresql-for-everybody/" target="_blank">free to University of Michigan Students</a>.
-</p></li>
-<li><p>
-<a href="https://www.cc4e.com" target="_blank">C Programming for Everybody (CC4E)</a>
-A course looking at the C language and its profound effect on modern computing and Computer Science.
-Available on Coursera, FreeCodeCamp, and
-<a href="https://online.umich.edu/series/c-programming-for-everybody/" target="_blank">
-free to University of Michigan Students</a>.
-</p></li>
-<li><p>
-<a href="https://www.ca4e.com" target="_blank">Computer Architecture for Everybody (CA4E)</a>
-- Under Construction
-</a>
-<li><p>
-Future course topics include JavaScript and Java.
-</p></li>
-</ul>
-</p>
+
+<div class="row-fluid">
+  <div class="span4">
+    <a href="https://ihts.pr4e.com/" target="_blank" class="course-card-link">
+      <div class="thumbnail course-card">
+        <img src="static/img/courses/ihts-card.png" alt="Internet History, Technology, and Security">
+        <div class="caption">
+          <h3>Internet History, Technology, and Security</h3>
+          <p>Covers history from early computing to the web, its commercialization, and key security concepts like encryption and digital signatures. Available on Coursera and FreeCodeCamp and free online for everyone.</p>
+          <p><span class="btn btn-primary">View Course</span></p>
+        </div>
+      </div>
+    </a>
+  </div>
+  
+  <div class="span4">
+    <a href="https://www.py4e.com/" target="_blank" class="course-card-link">
+      <div class="thumbnail course-card">
+        <img src="static/img/courses/py4e-card.png" alt="Python for Everybody">
+        <div class="caption">
+          <h3>Python for Everybody (PY4E)</h3>
+          <p>The world's most successful online Python course. Available on Coursera, edX, FreeCodeCamp, and free online for everyone.</p>
+          <p><span class="btn btn-primary">View Course</span></p>
+        </div>
+      </div>
+    </a>
+  </div>
+  
+  <div class="span4">
+    <a href="https://www.dj4e.com/" target="_blank" class="course-card-link">
+      <div class="thumbnail course-card">
+        <img src="static/img/courses/dj4e-card.png" alt="Django for Everybody">
+        <div class="caption">
+          <h3>Django for Everybody (DJ4E)</h3>
+          <p>Covers Django, HTML, CSS, SQL, JavaScript, BootStrap, and JQuery. Available on Coursera, edX, FreeCodeCamp and free online for everyone.</p>
+          <p><span class="btn btn-primary">View Course</span></p>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<div class="row-fluid">
+  <div class="span4">
+    <a href="https://www.wa4e.com/" target="_blank" class="course-card-link">
+      <div class="thumbnail course-card">
+        <img src="static/img/courses/wa4e-card.png" alt="Web Applications for Everybody">
+        <div class="caption">
+          <h3>Web Applications for Everybody (WA4E)</h3>
+          <p>Covers HTML, CSS, PHP, SQL, JavaScript, BootStrap, and JQuery. Available on Coursera, FreeCodeCamp, and free online for everyone.</p>
+          <p><span class="btn btn-primary">View Course</span></p>
+        </div>
+      </div>
+    </a>
+  </div>
+  
+  <div class="span4">
+    <a href="https://www.pg4e.com" target="_blank" class="course-card-link">
+      <div class="thumbnail course-card">
+        <img src="static/img/courses/pg4e-card.png" alt="PostgreSQL for Everybody">
+        <div class="caption">
+          <h3>PostgreSQL for Everybody (PG4E)</h3>
+          <p>A series of four courses covering PostgreSQL. Available on Coursera, edX, and free online for everyone.</p>
+          <p><span class="btn btn-primary">View Course</span></p>
+        </div>
+      </div>
+    </a>
+  </div>
+  
+  <div class="span4">
+    <a href="https://www.cc4e.com" target="_blank" class="course-card-link">
+      <div class="thumbnail course-card">
+        <img src="static/img/courses/cc4e-card.png" alt="C Programming for Everybody">
+        <div class="caption">
+          <h3>C Programming for Everybody (CC4E)</h3>
+            <p>A course looking at the C language and its profound effect on modern computing and Computer Science. Available on Coursera, FreeCodeCamp, and free online for everyone.</p>
+          <p><span class="btn btn-primary">View Course</span></p>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<div class="row-fluid">
+  <div class="span4">
+    <a href="https://www.ca4e.com" target="_blank" class="course-card-link">
+      <div class="thumbnail course-card">
+        <img src="static/img/courses/ca4e-card.png" alt="Computer Architecture for Everybody">
+        <div class="caption">
+          <h3>Computer Architecture for Everybody (CA4E)</h3>
+          <p>Rolling out Early 2026. This course covers digital electronics, how electronics can be used for computation, what machine language is, and what assembly language is, and how compiled languages like C work.</p>
+          <p><span class="btn btn-primary">View Course</span></p>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
 <p>
 The University of Michigan has a wide range of additional open online courses available at
 <a href="https://online.umich.edu" target="_blank">https://online.umich.edu</a>.
